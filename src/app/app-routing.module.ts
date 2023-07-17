008be84bd2040ba6e0af389e139df2f5646354f2
IT21191688 <IT21191688@my.sliit.lk>
2023-07-17 21:27:10 +0530

    ui structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', redirectTo:'starter',pathMatch:'full'},
+{ path: 'security', loadChildren: () => import('./module/security/security.module').then(m => m.SecurityModule) },
+{ path: 'starter', loadChildren: () => import('./module/starter/starter.module').then(m => m.StarterModule) },
+{path:'**', component:NotFoundPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecurityModule } from './module/security/security.module';
+import { StarterModule } from './module/starter/starter.module';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { StarterComponent } from './module/starter/starter.component';
+import { StarterHeaderComponent } from './components/starter-header/starter-header.component';
+import { StarterFooterComponent } from './components/starter-footer/starter-footer.component';
+import { StarterMainSliderComponent } from './components/starter-main-slider/starter-main-slider.component';
+import { StarterMainServicesComponent } from './components/starter-main-services/starter-main-services.component';
+import { ConsoleHeaderComponent } from './components/console-header/console-header.component';
+import { ConsoleFooterComponent } from './components/console-footer/console-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    NotFoundPageComponent,
+    AppComponent,
+    StarterHeaderComponent,
+    StarterFooterComponent,
+    StarterMainSliderComponent,
+    StarterMainServicesComponent,
+    ConsoleHeaderComponent,
+    ConsoleFooterComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SecurityModule,
+    StarterModule
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [StarterComponent]
 })
 export class AppModule { }
